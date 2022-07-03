e4e0cd6b374e376ba7d7a8d2b29f2914e56cf177
Gron <ahesteaheste0101@email.com>
2022-07-03 06:01:44 +0300

    Has been userId control fro getUserData method & 1.0.2

--- src/methods/getUserData/index.js
+++ src/methods/getUserData/index.js
@@ -2,12 +2,19 @@ import { axios, container } from "services";
 import { endPoints } from "_constants";
 
 const getUserData = async (userId) => {
-    const data = await axios.get(endPoints.getProfile, {
-        headers: {
-            "x-access-token": container.get(userId)
-        }
-    }).then((res) => res.data);
-    return data;
+    if (container.get(userId)) {
+        const data = await axios.get(endPoints.getProfile, {
+            headers: {
+                "x-access-token": container.get(userId)
+            }
+        }).then((res) => res.data);
+        return data;
+    }
+    
+    return {
+        code: 400,
+        message: "UserId not found in container."
+    }
 };
 
 export default getUserData;
